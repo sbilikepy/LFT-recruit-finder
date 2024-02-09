ae592faa6cb91886cb73e0d7eaff40c9f143a50f
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-02-09 18:55:59 +0200

    url setup for base urls and LFTPlatform urls

--- LFTplatform/views.py
+++ LFTplatform/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
 
+# from django.contrib.auth.decorators import login_required
+# from django.contrib.auth.mixins import LoginRequiredMixin
+# from django.shortcuts import render
+# from django.urls import reverse_lazy
+# from django.views import generic
+# from models import *
+# from .forms import *
+
+def index(request):
+    """View function for the home page of the site."""
+    return HttpResponse(":)")
