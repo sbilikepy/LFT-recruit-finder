6c9bfbc270a57f5df30b3ab1ffa3f25e4a6aa5a2
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-02-12 17:55:33 +0200

    index.html + view update. counters has been added

--- LFTplatform/views.py
+++ LFTplatform/views.py
@@ -1,14 +1,23 @@
-from django.http import HttpResponse
-
+# from django.http import HttpResponse
 
 # from django.contrib.auth.decorators import login_required
 # from django.contrib.auth.mixins import LoginRequiredMixin
-# from django.shortcuts import render
+from django.shortcuts import render
+
 # from django.urls import reverse_lazy
 # from django.views import generic
-# from models import *
-# from .forms import *
+from .models import *
+
 
 def index(request):
     """View function for the home page of the site."""
-    return HttpResponse(":)")
+
+    return render(
+        request,
+        template_name="LFTplatform/index.html",
+        context={
+            "guilds_count": Guild.objects.count(),
+            "teams_count": Team.objects.count(),
+            "recruits_count": Recruit.objects.count(),
+        }
+    )
