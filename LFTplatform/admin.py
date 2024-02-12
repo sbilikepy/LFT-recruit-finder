4cbd305cc2caac4a2b6c3b6a5686ebe6f6016d36
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-02-12 19:46:24 +0200

    db error

--- LFTplatform/admin.py
+++ LFTplatform/admin.py
@@ -1,83 +1,83 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-
-from .models import *
-
-
-@admin.register(Recruiter)
-class RecruiterAdmin(UserAdmin):
-    list_display = UserAdmin.list_display
-    fieldsets = UserAdmin.fieldsets
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (
-            (
-                "Additional info",
-                {
-                    "fields": (
-                        "first_name",
-                    )
-                },
-            ),
-        )
-    )
-
-
-@admin.register(Recruit)
-class RecruitAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        #"activity_days_recruit",
-        "activity_time_start",
-        "activity_time_end",
-        "note",
-    )
-    search_fields = ("name",)
-    list_filter = ("name",
-                   #"activity_days_recruit",
-                   "activity_time_start",
-                   "activity_time_end"
-                   )
-
-
-@admin.register(Character)
-class CharacterAdmin(admin.ModelAdmin):
-    list_display = (
-
-        "nickname",
-        "owner",
-        "class_name",
-        "spec_name",
-        "item_lvl",
-        "wcl"
-
-    )
-    search_fields = ("nickname",)
-    list_filter = ("spec_name", "class_name")
-
-
-@admin.register(Guild)
-class GuildAdmin(admin.ModelAdmin):
-    list_display = (
-        "name", "faction", "recruiter", "highest_progress", "apply_link",
-        "wcl_link", "guild_note",
-    )
-    search_fields = ("name",)
-    list_filter = ("name", "highest_progress")
-
-
-@admin.register(Team)
-class TeamAdmin(admin.ModelAdmin):
-    list_display = (
-        "guild",
-        "name",
-        "loot_system",
-        "team_size",
-        "team_progress",
-        # "activity_days_team",
-        "activity_time_start",
-        "activity_time_end",
-    )
-    search_fields = ("name", "guild")
-    list_filter = ("team_progress", "loot_system",
-                 #  "activity_days_team",
-                   )
+# from django.contrib import admin
+# from django.contrib.auth.admin import UserAdmin
+#
+# from .models import *
+#
+#
+# @admin.register(Recruiter)
+# class RecruiterAdmin(UserAdmin):
+#     list_display = UserAdmin.list_display
+#     fieldsets = UserAdmin.fieldsets
+#     add_fieldsets = UserAdmin.add_fieldsets + (
+#         (
+#             (
+#                 "Additional info",
+#                 {
+#                     "fields": (
+#                         "first_name",
+#                     )
+#                 },
+#             ),
+#         )
+#     )
+#
+#
+# @admin.register(Recruit)
+# class RecruitAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "name",
+#         # "activity_days_recruit",
+#         "activity_time_start",
+#         "activity_time_end",
+#         "note",
+#     )
+#     search_fields = ("name",)
+#     list_filter = ("name",
+#                    # "activity_days_recruit",
+#                    "activity_time_start",
+#                    "activity_time_end"
+#                    )
+#
+#
+# @admin.register(Character)
+# class CharacterAdmin(admin.ModelAdmin):
+#     list_display = (
+#
+#         "nickname",
+#         "owner",
+#         "class_name",
+#         "spec_name",
+#         "item_lvl",
+#         "wcl"
+#
+#     )
+#     search_fields = ("nickname",)
+#     list_filter = ("spec_name", "class_name")
+#
+#
+# @admin.register(Guild)
+# class GuildAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "name", "faction", "recruiter", "highest_progress", "apply_link",
+#         "wcl_link", "guild_note",
+#     )
+#     search_fields = ("name",)
+#     list_filter = ("name", "highest_progress")
+#
+#
+# @admin.register(Team)
+# class TeamAdmin(admin.ModelAdmin):
+#     list_display = (
+#         "guild",
+#         "name",
+#         "loot_system",
+#         "team_size",
+#         "team_progress",
+#         # "activity_days_team",
+#         "activity_time_start",
+#         "activity_time_end",
+#     )
+#     search_fields = ("name", "guild")
+#     list_filter = ("team_progress", "loot_system",
+#                    #  "activity_days_team",
+#                    )
--- LFTplatform/models.py
+++ LFTplatform/models.py
@@ -11,8 +11,8 @@ class Recruiter(AbstractUser):
         verbose_name_plural = "recruiters"
         verbose_name = "recruiter"
         ordering = ["username"]
-
-
+#
+#
 class ActivityDay(models.Model):
     """
     Represents a day of the week for specifying activity schedule
@@ -28,138 +28,134 @@ class ActivityDay(models.Model):
         ("sun", "Sunday"),
     ]
     day_of_week = models.CharField(max_length=3, choices=DAY_CHOICES)
-
-
-class Recruit(models.Model):
-    """
-    Represents a recruit, who looking for team
-    """
-
-    name = models.CharField(max_length=16)
-    note = models.CharField(max_length=512)
-
-    activity_days_recruit = models.ManyToManyField(ActivityDay)
-    activity_time_start = models.TimeField()
-    activity_time_end = models.TimeField()
-
-    class Meta:
-        pass
-
-    def __str__(self):
-        return f"{self.name}"
-
-
-class Character(models.Model):
-    """
-    Represents a character owned by a recruit
-    """
-
-    owner = models.ForeignKey(Recruit, on_delete=models.CASCADE)
-
-    nickname = models.CharField(max_length=12)
-
-    class_name = models.CharField(max_length=16)
-    spec_name = models.CharField(max_length=16)
-    item_lvl = models.IntegerField()
-
-    wcl_show = models.BooleanField()
-    wcl = models.URLField()
-
-    class Meta:
-        verbose_name_plural = "characters"
-        ordering = ["spec_name"]
-
-    def __str__(self):
-        return f"{self.spec_name} {self.class_name}"
-
-
-class Guild(models.Model):
-    """
-    Represents a gaming guild with associated recruiter
-    """
-
-    name = models.CharField(
-        max_length=24,
-        blank=False,
-        null=False,
-        unique=True
-    )
-    FACTION_CHOICES = [
-        ("alliance", "Alliance"),
-        ("horde", "Horde"),
-    ]
-    faction = models.CharField(
-        choices=FACTION_CHOICES,
-        max_length=8,
-        blank=False,
-        null=False,
-    )
-
-    recruiter = models.ForeignKey(
-        Recruiter, on_delete=models.CASCADE, related_name="recruiter"
-    )
-    highest_progress = models.IntegerField(default=0)
-    apply_link = models.URLField()
-    wcl_link = models.URLField()
-    guild_note = models.CharField(max_length=4096)
-
-    class Meta:
-        verbose_name_plural = "guilds"
-        ordering = ["?"]
-
-    def __str__(self):
-        return f"{self.name}"
-
-
-class Team(models.Model):
-    """
-    Represents a team as part of guild with activity schedule
-    """
-
-    guild = models.ForeignKey(
-        "Guild",
-        on_delete=models.CASCADE,
-        related_name="guild",
-    )
-    name = models.CharField(
-        max_length=24,
-        unique=False,
-    )
-    loot_system = models.CharField(  # TODO: choices / form validation
-        max_length=16,
-        default="Undefined",
-    )
-    team_size = models.IntegerField()
-    team_progress = models.IntegerField(default=0)
-
-    activity_days_team = models.ManyToManyField(ActivityDay)
-    activity_time_start = models.TimeField()
-    activity_time_end = models.TimeField()
-
-    class Meta:
-        verbose_name_plural = "teams"
-        constraints = [
-            models.UniqueConstraint(
-                fields=["name", "guild"],
-                name="unique_team_name_for_each_guild"
-            ),
-            models.UniqueConstraint(
-                fields=["name", "guild", "activity_days_team"],
-                name="unique_activity_time_for_each_team_and_guild"
-            ),
-        ]
-
-    def clean(self):
-        if not self.name:
-            self.name = (
-                f"{self.guild.name} team"
-                f"№ {Team.objects.filter(guild=self.guild).count() + 1}"
-            )
-
-        if self.team_progress > self.guild.highest_progress:
-            self.guild.highest_progress = self.team_progress
-
-        self.guild.save()
-
-    def __str__(self):
-        return f"{self.name}"
+#
+#
+# class Recruit(models.Model):
+#     """
+#     Represents a recruit, who looking for team
+#     """
+#
+#     name = models.CharField(max_length=16)
+#     note = models.CharField(max_length=512)
+#
+#     activity_days_recruit = models.ManyToManyField(ActivityDay)
+#     activity_time_start = models.TimeField()
+#     activity_time_end = models.TimeField()
+#
+#     class Meta:
+#         pass
+#
+#     def __str__(self):
+#         return f"{self.name}"
+#
+#
+# class Character(models.Model):
+#     """
+#     Represents a character owned by a recruit
+#     """
+#
+#     owner = models.ForeignKey(Recruit, on_delete=models.CASCADE)
+#
+#     nickname = models.CharField(max_length=12)
+#
+#     class_name = models.CharField(max_length=16)
+#     spec_name = models.CharField(max_length=16)
+#     item_lvl = models.IntegerField()
+#
+#     wcl_show = models.BooleanField()
+#     wcl = models.URLField()
+#
+#     class Meta:
+#         verbose_name_plural = "characters"
+#         ordering = ["spec_name"]
+#
+#     def __str__(self):
+#         return f"{self.spec_name} {self.class_name}"
+#
+#
+# class Guild(models.Model):
+#     """
+#     Represents a gaming guild with associated recruiter
+#     """
+#
+#     guild_name = models.CharField(
+#         max_length=24,
+#         blank=False,
+#         null=False,
+#         unique=True
+#     )
+#     FACTION_CHOICES = [
+#         ("alliance", "Alliance"),
+#         ("horde", "Horde"),
+#     ]
+#     faction = models.CharField(
+#         choices=FACTION_CHOICES,
+#         max_length=8,
+#         blank=False,
+#         null=False,
+#     )
+#
+#     recruiter = models.ForeignKey(
+#         Recruiter, on_delete=models.CASCADE, related_name="recruiter"
+#     )
+#     highest_progress = models.IntegerField(default=0)
+#     apply_link = models.URLField()
+#     wcl_link = models.URLField()
+#     guild_note = models.CharField(max_length=4096)
+#
+#     class Meta:
+#         verbose_name_plural = "guilds"
+#         ordering = ["?"]
+#
+#     def __str__(self):
+#         return f"{self.name}"
+#
+#
+# class Team(models.Model):
+#     """
+#     Represents a team as part of guild with activity schedule
+#     """
+#
+#     guild = models.ForeignKey(
+#         "Guild",
+#         on_delete=models.CASCADE,
+#         related_name="guild",
+#     )
+#     team_name = models.CharField(
+#         max_length=24,
+#         unique=False,
+#     )
+#     loot_system = models.CharField(  # TODO: choices / form validation
+#         max_length=16,
+#         default="Undefined",
+#     )
+#     team_size = models.IntegerField()
+#     team_progress = models.IntegerField(default=0)
+#
+#     activity_days_team = models.ManyToManyField(ActivityDay)
+#     activity_time_start = models.TimeField()
+#     activity_time_end = models.TimeField()
+#
+#     class Meta:
+#         verbose_name_plural = "teams"
+#         # constraints = [
+#         #     models.UniqueConstraint(
+#         #         fields=["team_name", "guild"],
+#         #         name="unique_team_name_for_each_guild"
+#         #     ),
+#         # ]
+#
+#     def clean(self):
+#         if not self.team_name:
+#             self.team_name = (
+#                 f"{self.guild.guild_name} team"
+#                 f"№ {Team.objects.filter(guild=self.guild).count() + 1}"
+#             )
+#
+#         if self.team_progress > self.guild.highest_progress:
+#             self.guild.highest_progress = self.team_progress
+#
+#         self.guild.save()
+#
+#     def __str__(self):
+#         return f"{self.team_name}"
