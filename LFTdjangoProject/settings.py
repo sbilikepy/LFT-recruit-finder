ccc5841612142e6e2f269627f09259f8e710cb90
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2024-02-21 18:43:31 +0200

    Added path to the static folder in Django settings by os.join

--- LFTdjangoProject/settings.py
+++ LFTdjangoProject/settings.py
@@ -79,7 +79,9 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = (BASE_DIR / "static",)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 AUTH_USER_MODEL = "LFTplatform.Recruiter"
